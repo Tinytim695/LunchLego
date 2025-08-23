183f08efa85f30224c4a0764dc13eecf6244a1a8
Tinytim695 <oisinmcfadden16@gmail.com>
2025-08-23 20:03:24 +0100

    Add App component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: 32 }}>
+      <h1>LunchLego</h1>
+      <p>Welcome to the 60-second drag-and-drop lunchbox planner web app!</p>
+    </div>
+  );
+}
+
+export default App;
