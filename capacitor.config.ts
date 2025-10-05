9e0f0ff0f239da7958d52428fa8021fb7076839e
Tinytim695 <oisinmcfadden16@gmail.com>
2025-10-05 11:08:48 +0100

    Added capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lunchlego.app',
+  appName: 'LunchLego',
+  webDir: 'dist'
+};
+
+export default config;
