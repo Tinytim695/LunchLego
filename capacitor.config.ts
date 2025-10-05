a5bef1d5de9804a487a0f29b8ff78aacfb6d3111
Tinytim695 <oisinmcfadden16@gmail.com>
2025-10-05 11:09:24 +0100

    Fix npm global package permission errors

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,19 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from '@capacitor/core';
 
 const config: CapacitorConfig = {
   appId: 'com.lunchlego.app',
   appName: 'LunchLego',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#0f172a",
+      showSpinner: false
+    }
+  }
 };
 
 export default config;
